70bb9d967452a2dbac12b0092250ae01928fbba1
bulletPr <qiangweiweiwei@gmail.com>
2020-11-14 15:01:48 +0100

    "bug fix"

--- datasets.py
+++ datasets.py
@@ -1,53 +1,102 @@
-'''
-    ModelNet dataset. Support ModelNet40, XYZ channels. Up to 2048 points.
-    Faster IO than ModelNetDataset in the first epoch.
-'''
-
+from __future__ import print_function
+import torch.utils.data as data
+from PIL import Image
 import os
-import sys
-import numpy as np
-import h5py
-from glob import glob
+import os.path
+import errno
 import torch
+import json
+import codecs
+import numpy as np
+import sys
+import torchvision.transforms as transforms
+import argparse
+import json
 
-dataset_path = 'modelnet40_ply_hdf5_2048'
-
-def load_h5(path):
-    all_data = []
-    all_label = []
-    for h5_name in path:
-        f = h5py.File(h5_name)
-        data = f['data'][:]
-        label = f['label'][:]
-        all_data.append(data)
-        all_label.append(label)
-    return (all_data, all_label)
-
-
-class ModelNetH5Dataset(object):
-    def __init__(self, root=dataset_path, npoints=2048, train=False):
-        self.root=root
+#Part Dataset
+class PartDataset(data.Dataset):
+    def __init__(self, root, npoints = 2500, classification = False, class_choice = None, train = True):
         self.npoints = npoints
-        self.h5_files = []
-        if(train):
-            list_filename = os.path.join(self.root, '*train*.h5')
-        else:
-            list_filename = os.path.join(self.root, '*train*.h5')
+        self.root = root
+        self.catfile = os.path.join(self.root, 'synsetoffset2category.txt')
+        self.cat = {}
 
-        self.h5_files += glob(list_filename)
-        data, labels = load_h5(self.h5_files)
-        self.data = np.concatenate(data,axis=0)
-        self.labels = np.concatenate(labels, axis=0)
+        self.classification = classification
+
+        with open(self.catfile, 'r') as f:
+            for line in f:
+                ls = line.strip().split()
+                self.cat[ls[0]] = ls[1]
+        #print(self.cat)
+        if not class_choice is  None:
+            self.cat = {k:v for k,v in self.cat.items() if k in class_choice}
+
+        self.meta = {}
+        for item in self.cat:
+            #print('category', item)
+            self.meta[item] = []
+            dir_point = os.path.join(self.root, self.cat[item], 'points')
+            dir_seg = os.path.join(self.root, self.cat[item], 'points_label')
+            #print(dir_point, dir_seg)
+            fns = sorted(os.listdir(dir_point))
+            if train:
+                fns = fns[:int(len(fns) * 0.9)]
+            else:
+                fns = fns[int(len(fns) * 0.9):]
+
+            #print(os.path.basename(fns))
+            for fn in fns:
+                token = (os.path.splitext(os.path.basename(fn))[0])
+                self.meta[item].append((os.path.join(dir_point, token + '.pts'), os.path.join(dir_seg, token + '.seg')))
+
+        self.datapath = []
+        for item in self.cat:
+            for fn in self.meta[item]:
+                self.datapath.append((item, fn[0], fn[1]))
+
+
+        self.classes = dict(zip(sorted(self.cat), range(len(self.cat))))
+        print(self.classes)
+        self.num_seg_classes = 0
+        if not self.classification:
+            for i in range(len(self.datapath)//50):
+                l = len(np.unique(np.loadtxt(self.datapath[i][-1]).astype(np.uint8)))
+                if l > self.num_seg_classes:
+                    self.num_seg_classes = l
+        #print(self.num_seg_classes)
+
 
     def __getitem__(self, index):
-        point_set = self.data[index]
-        label = self.labels[index]
+        fn = self.datapath[index]
+        cls = self.classes[self.datapath[index][0]]
+        point_set = np.loadtxt(fn[1]).astype(np.float32)
+        seg = np.loadtxt(fn[2]).astype(np.int64)
         #print(point_set.shape, seg.shape)
 
+        choice = np.random.choice(len(seg), self.npoints, replace=True)
+        #resample
+        point_set = point_set[choice, :]
+        seg = seg[choice]
         point_set = torch.from_numpy(point_set)
-        label = torch.from_numpy(np.array([label]).astype(np.int64))
-        return point_set, label
-
+        seg = torch.from_numpy(seg)
+        cls = torch.from_numpy(np.array([cls]).astype(np.int64))
+        if self.classification:
+            return point_set, cls
+        else:
+            return point_set, seg
 
     def __len__(self):
-        return self.data.shape[0]
+        return len(self.datapath)
+
+
+if __name__ == '__main__':
+    print('test')
+    d = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', class_choice = ['Chair'])
+    print(len(d))
+    ps, seg = d[0]
+    print(ps.size(), ps.type(), seg.size(),seg.type())
+
+    d = PartDataset(root = 'shapenetcore_partanno_segmentation_benchmark_v0', classification = True)
+    print(len(d))
+    ps, cls = d[0]
+    print(ps.size(), ps.type(), cls.size(),cls.type())
---
+++ modelnet40_loader.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import numpy as np
+import h5py
+from glob import glob
+import torch
+
+dataset_path = 'modelnet40_ply_hdf5_2048'
+
+def load_h5(path):
+    all_data = []
+    all_label = []
+    for h5_name in path:
+        f = h5py.File(h5_name)
+        data = f['data'][:]
+        label = f['label'][:]
+        all_data.append(data)
+        all_label.append(label)
+    return (all_data, all_label)
+
+
+class ModelNetH5Dataset(object):
+    def __init__(self, root=dataset_path, npoints=2048, train=False):
+        self.root=root
+        self.npoints = npoints
+        self.h5_files = []
+        if(train):
+            list_filename = os.path.join(self.root, '*train*.h5')
+        else:
+            list_filename = os.path.join(self.root, '*test*.h5')
+
+        self.h5_files += glob(list_filename)
+        data, labels = load_h5(self.h5_files)
+        self.data = np.concatenate(data,axis=0)
+        self.labels = np.concatenate(labels, axis=0)
+
+    def __getitem__(self, index):
+        point_set = self.data[index]
+        label = self.labels[index]
+        #print(point_set.shape, seg.shape)
+
+        point_set = torch.from_numpy(point_set)
+        label = torch.from_numpy(np.array([label]).astype(np.int64))
+        return point_set, label
+
+
+    def __len__(self):
+        return self.data.shape[0]
